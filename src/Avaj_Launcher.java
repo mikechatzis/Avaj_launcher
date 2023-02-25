026d239ff0976b3256de8ba69f16c05e5d9917c3
Michail Chatzipanagiotou <chtzmike@gmail.com>
2023-02-25 17:43:01 +0100

    functioning, further debugging needed

---
+++ src/Avaj_Launcher.java
@@ -0,0 +1,20 @@
+
+public class Avaj_Launcher {
+    public static void main(String[] args) throws Exception {
+        try {
+            if (args.length != 1)
+                throw new customException("wrong number of arguments. Format: 'avaj_launcher <path_to_scenario>'");
+            fileReader R = new fileReader(args[0]);
+            R.createAircrafts();
+            R.scenarioLoop();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+        finally {
+            fileWriter.printWriter.close();
+        }
+    }
+}
